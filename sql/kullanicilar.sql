28bdc3f69e2f8b4ef9bf00be1f3ba343a43a9343
bgozdag <bgozdag@gmail.com>
2015-08-24 20:26:17 +0300

    Create kullanicilar.sql

---
+++ sql/kullanicilar.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: proje2
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `kullanicilar`
+--
+
+DROP TABLE IF EXISTS `kullanicilar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kullanicilar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `isim` varchar(45) NOT NULL,
+  `soyisim` varchar(45) NOT NULL,
+  `meslek` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `tarih` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kullanicilar`
+--
+
+LOCK TABLES `kullanicilar` WRITE;
+/*!40000 ALTER TABLE `kullanicilar` DISABLE KEYS */;
+INSERT INTO `kullanicilar` VALUES (60,'adssadsd','wrer','Öğrenci','trtrt','2015-08-14 13:32:19'),(61,'dffdfd','sadada','Stajyer','cvvc','2015-08-14 13:32:28'),(62,'gfgdgdgd','cvvcvc','Geliştirici','sddsds','2015-08-14 13:33:12');
+/*!40000 ALTER TABLE `kullanicilar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-08-24 20:06:43
